77aef27b3f3b2ecbb94b08100b7302fecf25e50f
Lucas Ramos <85357089+Lucas-RamosDev@users.noreply.github.com>
2021-07-16 11:20:36 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ExperienceBar } from './components/experienceBar'
+
+import './styles/global.css';
+
+
+function App() {
+  return (
+
+    <div className="container">
+      <ExperienceBar />
+    </div>
+
+  );
+}
+
+export default App;
